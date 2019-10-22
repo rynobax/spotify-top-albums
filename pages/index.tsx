64cbb715dd8841b9d5d773b3c55da4e15f7358f8
Ryan Baxley <ryan.baxley1@gmail.com>
2019-10-21 22:31:31 -0400

    Change to cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,39 +19,43 @@ const Home: NextComponentType<NextPageContext, InitialProps, InitialProps> = ({
         <title>Home</title>
         <link rel="icon" href="/public/favicon.ico" />
         <link
-          href="https://fonts.googleapis.com/css?family=Titillium+Web"
+          href="https://fonts.googleapis.com/css?family=Titillium+Web:400,700"
           rel="stylesheet"
         ></link>
       </Head>
 
-      <div className="container mx-auto md mt-8 shadow rounded">
+      <div className="container mx-auto md mt-8 flex flex-row flex-wrap">
         {playlist.map((track, ndx) => {
           console.log({ track });
           const rank = ndx + 1;
           return (
             <div
-              className="flex flex-row items-center rounded my-4 py-4 h-24"
+              className="rounded my-4 rounded-lg flex-shrink-0 bg-gray-100 w-64 overflow-hidden mx-8"
               key={track.album}
             >
-              <div className="flex-initial w-12 bg-gray-300 m-h-full shadow-md h-full flex flex-row items-center rounded-l justify-center">
-                <div className="">
-                  <span className="text-xl">#</span>
-                  <span className="text-4xl">{rank}</span>
+              <img className="flex-initial w-64 h-64" src={track.img} />
+              <div className="p-2">
+                <div className="flex flex-row align-middle">
+                  <div className="flex flex-col flex-0 h-full justify-center">
+                    <div className="flex-grow-0">
+                      <span className="text-xl">#</span>
+                      <span className="text-4xl">{rank}</span>
+                    </div>
+                  </div>
+                  <div className="flex-0 flex flex-col pl-2">
+                    <div className="h-24 text-xl text-gray-800 font-bold leading-tight">
+                      {track.album}
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="flex-1 flex flex-row bg-gray-300 ml-4 items-center shadow-md h-full rounded-r overflow-hidden">
-                <img className="flex-initial h-full" src={track.img} />
-                <div className="flex-1 flex flex-row text-xl">
-                  <div className="flex-2">{track.album}</div>
-                  <div className="flex-1">{track.artist}</div>
-                </div>
+                <div className="h-6 text-xl text-gray-600">{track.artist}</div>
               </div>
             </div>
           );
         })}
       </div>
 
-      <div className="absolute bg-gray-800 inset-0" style={{ zIndex: -1 }} />
+      <div className="fixed bg-gray-800 inset-0" style={{ zIndex: -1 }} />
     </div>
   );
 };
