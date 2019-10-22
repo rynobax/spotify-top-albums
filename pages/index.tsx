e9efb48ac9756a62a02a7d78b4a0631b53eff1e3
Ryan Baxley <ryan.baxley1@gmail.com>
2019-10-21 21:18:10 -0400

    Initial stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,18 @@
-import React from 'react';
-import Head from 'next/head';
-import { NextComponentType, NextPageContext } from 'next';
-import { getPlaylist } from '../spotify';
-import { PlaylistResponse } from '../spotify/types/playlist';
+import React from "react";
+import Head from "next/head";
+import { NextComponentType, NextPageContext } from "next";
 
-import '../styles/index.css';
+import { getPlaylist, Playlist } from "../spotify";
 
-const qualifiers = [' (Deluxe)', ' (Extended)'];
-const removeQualifiers = (name: string) =>
-  qualifiers.reduce((n, q) => n.replace(q, ''), name);
+import "../styles/index.css";
 
 interface InitialProps {
-  playlist: PlaylistResponse;
+  playlist: Playlist;
 }
 
 const Home: NextComponentType<NextPageContext, InitialProps, InitialProps> = ({
-  playlist
+  playlist,
 }) => {
-  console.log(playlist);
-
-  const tracks = playlist.items
-    .map(item => item.track)
-    .map(({ album, artists }) => ({
-      artist: artists[0].name,
-      album: removeQualifiers(album.name),
-      img: album.images[1].url,
-      uri: album.uri
-    }));
-
   return (
     <div>
       <Head>
@@ -40,10 +25,14 @@ const Home: NextComponentType<NextPageContext, InitialProps, InitialProps> = ({
       </Head>
 
       <div className="container mx-auto md mt-8 shadow rounded">
-        {tracks.map((track, ndx) => {
+        {playlist.map((track, ndx) => {
+          console.log({ track });
           const rank = ndx + 1;
           return (
-            <div className="flex flex-row items-center rounded my-4 py-4 h-24">
+            <div
+              className="flex flex-row items-center rounded my-4 py-4 h-24"
+              key={track.album}
+            >
               <div className="flex-initial w-12 bg-gray-300 m-h-full shadow-md h-full flex flex-row items-center rounded-l justify-center">
                 <div className="">
                   <span className="text-xl">#</span>
@@ -68,7 +57,7 @@ const Home: NextComponentType<NextPageContext, InitialProps, InitialProps> = ({
 };
 
 Home.getInitialProps = async () => {
-  const playlist = await getPlaylist('1D9hvrANrHxVGGVx49v4x1');
+  const playlist = await getPlaylist("1D9hvrANrHxVGGVx49v4x1");
   return { playlist };
 };
 
--- spotify/index.ts
+++ spotify/index.ts
@@ -1,16 +1,36 @@
-import axios from 'axios';
-import { PlaylistResponse } from './types/playlist';
+import axios from "axios";
+const Vibrant: TVibrant = require("node-vibrant");
+
+type Palette = Record<
+  | "DarkMuted"
+  | "DarkVibrant"
+  | "DarkMuted"
+  | "LightMuted"
+  | "Muted"
+  | "Vibrant",
+  { rgb: [number, number, number] }
+>;
+
+interface VibrantInstance {
+  getPalette: () => Promise<Palette>;
+}
+
+interface TVibrant {
+  from: (url: string) => VibrantInstance;
+}
+
+import { PlaylistResponse } from "./types/playlist";
 
 // albums: 1D9hvrANrHxVGGVx49v4x1
 
 const ID = process.env.ID;
 const SECRET = process.env.SECRET;
 
-const inNode = typeof window === 'undefined';
+const inNode = typeof window === "undefined";
 
 function b64Encode(str: string) {
   if (inNode) {
-    return Buffer.from(str).toString('base64');
+    return Buffer.from(str).toString("base64");
   } else {
     return btoa(str);
   }
@@ -19,17 +39,17 @@ function b64Encode(str: string) {
 const BASIC_AUTH = b64Encode(`${ID}:${SECRET}`);
 
 const spotifyAuth = axios.create({
-  baseURL: 'https://accounts.spotify.com/api/'
+  baseURL: "https://accounts.spotify.com/api/",
 });
 
-if (!ID || !SECRET) throw Error('Did not find ID or SECRET, check .env');
+if (!ID || !SECRET) throw Error("Did not find ID or SECRET, check .env");
 
 async function getAccessTokenFromSpotify() {
-  const res = await spotifyAuth.post('token', 'grant_type=client_credentials', {
+  const res = await spotifyAuth.post("token", "grant_type=client_credentials", {
     headers: {
       Authorization: `Basic ${BASIC_AUTH}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
   });
   return res.data.access_token;
 }
@@ -40,15 +60,38 @@ async function getClient() {
     token = await getAccessTokenFromSpotify();
   }
   return axios.create({
-    baseURL: 'https://api.spotify.com/v1/',
-    headers: { Authorization: `Bearer ${token}` }
+    baseURL: "https://api.spotify.com/v1/",
+    headers: { Authorization: `Bearer ${token}` },
   });
 }
 
+const qualifiers = [" (Deluxe)", " (Extended)"];
+const removeQualifiers = (name: string) =>
+  qualifiers.reduce((n, q) => n.replace(q, ""), name);
+
+type ExtractPromise<T> = T extends Promise<infer U> ? U : never;
+export type Playlist = ExtractPromise<ReturnType<typeof getPlaylist>>;
+
 export async function getPlaylist(playlistId: string) {
   const spotify = await getClient();
-  const { data } = await spotify.get<PlaylistResponse>(
+  const { data: playlist } = await spotify.get<PlaylistResponse>(
     `playlists/${playlistId}/tracks`
   );
-  return data;
+  const tracks = await Promise.all(
+    playlist.items
+      .map(item => item.track)
+      .map(async ({ album, artists }) => {
+        const v = Vibrant.from(album.images[1].url);
+        const palette = await v.getPalette();
+        return {
+          artist: artists[0].name,
+          album: removeQualifiers(album.name),
+          img: album.images[1].url,
+          uri: album.uri,
+          palette,
+        };
+      })
+  );
+
+  return tracks;
 }
